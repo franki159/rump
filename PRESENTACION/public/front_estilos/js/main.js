4e2a11ffb7b5ce7bcf5e3aa03069d03003583b4f
frankjgp <frank.jgp14@gmail.com>
2019-10-24 12:43:22 -0500

    mapa convenio

--- PRESENTACION/public/front_estilos/js/main.js
+++ PRESENTACION/public/front_estilos/js/main.js
@@ -1,168 +1,269 @@
-jQuery(document).ready(function( $ ) {
+var _ListaTipoConvenio, _ListaConvenio, _LatitudActual, _LongitudActual;
 
-  // Back to top button
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      $('.back-to-top').fadeIn('slow');
-    } else {
-      $('.back-to-top').fadeOut('slow');
-    }
-  });
-  $('.back-to-top').click(function(){
-    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
-    return false;
-  });
+jQuery(document).ready(function ($) {
 
-  // Stick the header at top on scroll
-  $("#header").sticky({topSpacing:0, zIndex: '50'});
-
-  // Intro background carousel
-  $("#intro-carousel").owlCarousel({
-    autoplay: true,
-    dots: false,
-    loop: true,
-    animateOut: 'fadeOut',
-    items: 1
-  });
-
-  // Initiate the wowjs animation library
-  new WOW().init();
-
-  // Initiate superfish on nav menu
-  $('.nav-menu').superfish({
-    animation: {
-      opacity: 'show'
-    },
-    speed: 400
-  });
-
-  // Mobile Navigation
-  if ($('#nav-menu-container').length) {
-    var $mobile_nav = $('#nav-menu-container').clone().prop({
-      id: 'mobile-nav'
-    });
-    $mobile_nav.find('> ul').attr({
-      'class': '',
-      'id': ''
-    });
-    $('body').append($mobile_nav);
-    $('body').prepend('<button type="button" id="mobile-nav-toggle"><i class="fa fa-bars"></i></button>');
-    $('body').append('<div id="mobile-body-overly"></div>');
-    $('#mobile-nav').find('.menu-has-children').prepend('<i class="fa fa-chevron-down"></i>');
-
-    $(document).on('click', '.menu-has-children i', function(e) {
-      $(this).next().toggleClass('menu-item-active');
-      $(this).nextAll('ul').eq(0).slideToggle();
-      $(this).toggleClass("fa-chevron-up fa-chevron-down");
-    });
-
-    $(document).on('click', '#mobile-nav-toggle', function(e) {
-      $('body').toggleClass('mobile-nav-active');
-      $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
-      $('#mobile-body-overly').toggle();
-    });
-
-    $(document).click(function(e) {
-      var container = $("#mobile-nav, #mobile-nav-toggle");
-      if (!container.is(e.target) && container.has(e.target).length === 0) {
-        if ($('body').hasClass('mobile-nav-active')) {
-          $('body').removeClass('mobile-nav-active');
-          $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
-          $('#mobile-body-overly').fadeOut();
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
         }
-      }
     });
-  } else if ($("#mobile-nav, #mobile-nav-toggle").length) {
-    $("#mobile-nav, #mobile-nav-toggle").hide();
-  }
-
-  // Smooth scroll for the menu and links with .scrollto classes
-  $('.nav-menu a, #mobile-nav a, .scrollto').on('click', function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      if (target.length) {
-        var top_space = 0;
-
-        if ($('#header').length) {
-          top_space = $('#header').outerHeight();
-
-          if( ! $('#header').hasClass('header-fixed') ) {
-            top_space = top_space - 20;
-          }
-        }
-
-        $('html, body').animate({
-          scrollTop: target.offset().top - top_space
-        }, 1500, 'easeInOutExpo');
-
-        if ($(this).parents('.nav-menu').length) {
-          $('.nav-menu .menu-active').removeClass('menu-active');
-          $(this).closest('li').addClass('menu-active');
-        }
-
-        if ($('body').hasClass('mobile-nav-active')) {
-          $('body').removeClass('mobile-nav-active');
-          $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
-          $('#mobile-body-overly').fadeOut();
-        }
+    $('.back-to-top').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
         return false;
-      }
+    });
+
+    // Stick the header at top on scroll
+    $("#header").sticky({ topSpacing: 0, zIndex: '50' });
+
+    // Intro background carousel
+    $("#intro-carousel").owlCarousel({
+        autoplay: true,
+        dots: false,
+        loop: true,
+        animateOut: 'fadeOut',
+        items: 1
+    });
+
+    // Initiate the wowjs animation library
+    new WOW().init();
+
+    // Initiate superfish on nav menu
+    $('.nav-menu').superfish({
+        animation: {
+            opacity: 'show'
+        },
+        speed: 400
+    });
+
+    // Mobile Navigation
+    if ($('#nav-menu-container').length) {
+        var $mobile_nav = $('#nav-menu-container').clone().prop({
+            id: 'mobile-nav'
+        });
+        $mobile_nav.find('> ul').attr({
+            'class': '',
+            'id': ''
+        });
+        $('body').append($mobile_nav);
+        $('body').prepend('<button type="button" id="mobile-nav-toggle"><i class="fa fa-bars"></i></button>');
+        $('body').append('<div id="mobile-body-overly"></div>');
+        $('#mobile-nav').find('.menu-has-children').prepend('<i class="fa fa-chevron-down"></i>');
+
+        $(document).on('click', '.menu-has-children i', function (e) {
+            $(this).next().toggleClass('menu-item-active');
+            $(this).nextAll('ul').eq(0).slideToggle();
+            $(this).toggleClass("fa-chevron-up fa-chevron-down");
+        });
+
+        $(document).on('click', '#mobile-nav-toggle', function (e) {
+            $('body').toggleClass('mobile-nav-active');
+            $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+            $('#mobile-body-overly').toggle();
+        });
+
+        $(document).click(function (e) {
+            var container = $("#mobile-nav, #mobile-nav-toggle");
+            if (!container.is(e.target) && container.has(e.target).length === 0) {
+                if ($('body').hasClass('mobile-nav-active')) {
+                    $('body').removeClass('mobile-nav-active');
+                    $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+                    $('#mobile-body-overly').fadeOut();
+                }
+            }
+        });
+    } else if ($("#mobile-nav, #mobile-nav-toggle").length) {
+        $("#mobile-nav, #mobile-nav-toggle").hide();
     }
-  });
+
+    // Smooth scroll for the menu and links with .scrollto classes
+    $('.nav-menu a, #mobile-nav a, .scrollto').on('click', function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            if (target.length) {
+                var top_space = 0;
+
+                if ($('#header').length) {
+                    top_space = $('#header').outerHeight();
+
+                    if (!$('#header').hasClass('header-fixed')) {
+                        top_space = top_space - 20;
+                    }
+                }
+
+                $('html, body').animate({
+                    scrollTop: target.offset().top - top_space
+                }, 1500, 'easeInOutExpo');
+
+                if ($(this).parents('.nav-menu').length) {
+                    $('.nav-menu .menu-active').removeClass('menu-active');
+                    $(this).closest('li').addClass('menu-active');
+                }
+
+                if ($('body').hasClass('mobile-nav-active')) {
+                    $('body').removeClass('mobile-nav-active');
+                    $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+                    $('#mobile-body-overly').fadeOut();
+                }
+                return false;
+            }
+        }
+    });
 
 
-  // Porfolio - uses the magnific popup jQuery plugin
-  $('.portfolio-popup').magnificPopup({
-    type: 'image',
-    removalDelay: 300,
-    mainClass: 'mfp-fade',
-    gallery: {
-      enabled: true
-    },
-    zoom: {
-      enabled: true,
-      duration: 300,
-      easing: 'ease-in-out',
-      opener: function(openerElement) {
-        return openerElement.is('img') ? openerElement : openerElement.find('img');
-      }
-    }
-  });
+    // Porfolio - uses the magnific popup jQuery plugin
+    $('.portfolio-popup').magnificPopup({
+        type: 'image',
+        removalDelay: 300,
+        mainClass: 'mfp-fade',
+        gallery: {
+            enabled: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 300,
+            easing: 'ease-in-out',
+            opener: function (openerElement) {
+                return openerElement.is('img') ? openerElement : openerElement.find('img');
+            }
+        }
+    });
 
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    responsive: { 0: { items: 1 }, 768: { items: 2 }, 900: { items: 3 } }
-  });
+    // Testimonials carousel (uses the Owl Carousel library)
+    $(".testimonials-carousel").owlCarousel({
+        autoplay: true,
+        dots: true,
+        loop: true,
+        responsive: { 0: { items: 1 }, 768: { items: 2 }, 900: { items: 3 } }
+    });
 
-  // Clients carousel (uses the Owl Carousel library)
-  $(".clients-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    responsive: { 0: { items: 2 }, 768: { items: 4 }, 900: { items: 6 }
-    }
-  });
+    // Clients carousel (uses the Owl Carousel library)
+    $(".clients-carousel").owlCarousel({
+        autoplay: true,
+        dots: true,
+        loop: true,
+        responsive: {
+            0: { items: 2 }, 768: { items: 4 }, 900: { items: 6 }
+        }
+    });
 
-  //Google Map
-  var get_latitude = $('#google-map').data('latitude');
-  var get_longitude = $('#google-map').data('longitude');
+    //Google Map
+    /*
+    var get_latitude = $('#google-map').data('latitude');
+    var get_longitude = $('#google-map').data('longitude');
 
-  function initialize_google_map() {
+    function initialize_google_map() {
     var myLatlng = new google.maps.LatLng(get_latitude, get_longitude);
     var mapOptions = {
-      zoom: 14,
-      scrollwheel: false,
-      center: myLatlng
+    zoom: 14,
+    scrollwheel: false,
+    center: myLatlng
     };
     var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
     var marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map
+    position: myLatlng,
+    map: map
     });
-  }
-  //google.maps.event.addDomListener(window, 'load', initialize_google_map);
+    }
+    */
+    //google.maps.event.addDomListener(window, 'load', initialize_google_map);
+    if ($('#mapa')) {
+        function showConvenio() {
+            var ubicaciones = [];
 
+            if (_LatitudActual != null && _LatitudActual != undefined) {
+                ubicaciones.push({ position: [_LatitudActual, _LongitudActual], icon: 'img/map_marker.png' });
+            }
+
+            if ($('#pnlTipoConvenio input').length == $('#pnlTipoConvenio input:checked').length) {
+                for (var i = 0; i < _ListaConvenio.length; i++) {
+                    ubicaciones.push({ position: [_ListaConvenio[i].LATITUD, _ListaConvenio[i].LONGITUD], icon: 'img/map_dog.png' });
+                }
+            } else {
+                $('#pnlTipoConvenio input:checked').each(function (a, b) {
+                    for (var i = 0; i < _ListaConvenio.length; i++) {
+                        if ($(b).val() == _ListaConvenio[i].TIPO_ID) {
+                            ubicaciones.push({ position: [_ListaConvenio[i].LATITUD, _ListaConvenio[i].LONGITUD], icon: 'img/map_dog.png' });
+                        }
+                    }
+                });
+            }
+
+            $('#mapa').gmap3({
+                action: 'init',
+                center: [
+                    (_LatitudActual != null || _LatitudActual != undefined ? _LatitudActual : -11.9637802),
+                    (_LongitudActual != null || _LongitudActual != undefined ? _LongitudActual : -77.072734)
+                ],
+                zoom: 10,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            });
+
+            $('#mapa').gmap3().marker(ubicaciones);
+
+            //$('#mapa').gmap3().marker([
+            //    { position: [_latitudActual, _longitudActual], icon: 'img/map_marker.png' },
+            //    { position: [-12.0326500, -76.87728], icon: 'img/map_dog.png' },
+            //    { position: [-11.79616, -76.97686], icon: 'img/map_dog.png' },
+            //    { position: [-11.889027, -77.038159], icon: 'img/map_dog.png' }
+            //]);
+        }
+
+        function showPosition(position) {
+            if (_ListaTipoConvenio != null && _ListaTipoConvenio != undefined) return false;
+
+            if (position != undefined) {
+                _LatitudActual = position.coords.latitude;
+                _LongitudActual = position.coords.longitude;
+            }
+
+            $.ajax({
+                type: "POST",
+                url: "indexPage.aspx/ListaMapa",
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                async: false,
+                beforeSend: function () {
+                    $("#pnlTipoConvenio").empty();
+                },
+                success: function (data) {
+                    _ListaTipoConvenio = data.d.Resultado.TIPOS;
+                    _ListaConvenio = data.d.Resultado.CONVENIOS;
+
+                    var html = '';
+                    for (var i = 0; i < _ListaTipoConvenio.length; i++) {
+                        html += '<label><input type="checkbox" checked="checked" value="' + _ListaTipoConvenio[i].ID + '" /> ' +
+                            _ListaTipoConvenio[i].TIPO + '</label><br />';
+                    }
+
+                    $("#pnlTipoConvenio").append(html);
+
+                    //$('#mapa').gmap3({
+                    //    center: [
+                    //        (_LatitudActual != null || _LatitudActual != undefined ? _LatitudActual : -11.9637802),
+                    //        (_LongitudActual != null || _LongitudActual != undefined ? _LongitudActual : -77.072734)
+                    //    ],
+                    //    zoom: 10,
+                    //    mapTypeId: google.maps.MapTypeId.ROADMAP
+                    //});
+
+                    showConvenio();
+                },
+                error: function (data) { }
+            });
+
+
+            $('#pnlTipoConvenio input').click(function () {
+                showConvenio();
+            });
+        }
+
+        if (navigator.geolocation) {
+            navigator.geolocation.watchPosition(showPosition);
+        } else {
+            showPosition();
+        }
+    }
 });
