cfd51f7984054a8aaa98186f3b95473d890de950
Frank <chara_20_90@hotmail.com>
2019-11-13 14:02:04 -0500

    solicitud v1

---
+++ PRESENTACION/page/mantenimiento/solicitud.aspx
@@ -0,0 +1,82 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="solicitud.aspx.cs" Inherits="PRESENTACION.page.mantenimiento.solicitud" %>
+
+<div id="errorDiv"></div>
+<input id="txh_idConfirm" type="hidden" />
+<input id="txh_idmovimiento" type="hidden" />
+<div class="row">
+    <div class="col-md-12">
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">Solicitudes y Trámites</h6>
+            </div>
+            <div class="card-body" id="pnl_busqueda">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="input-group">
+                            <label class="col-form-label">E-mail: </label>
+                            <input type="text" id="txt_bus_email" class="form-control bg-light border-0 small" placeholder="Buscar usuario..." aria-label="Search" aria-describedby="basic-addon2">
+                            <div class="input-group-append">
+                                <button id="btn_buscars" class="btn btn-primary" type="button">
+                                    <i class="fas fa-search fa-sm"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <button id="btn_buscar" class="btn btn-secondary btn-icon-split">
+                            <span class="icon text-white-50">
+                                <i class="fas fa-file"></i>
+                            </span>
+                            <span class="text">Nuevo</span>
+                        </button>
+                        <button id="btn_nuevo" class="btn btn-secondary btn-icon-split">
+                            <span class="icon text-white-50">
+                                <i class="fas fa-file"></i>
+                            </span>
+                            <span class="text">Nuevo</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="card shadow mb-4">
+            <div class="panel pre-scrollable">
+                <table id="tbl_usuario" class="table table-striped table-hover table-fcp">
+                    <thead>
+                        <tr>
+                            <th style="display: none"></th>
+                            <th></th>
+                            <th>Tipo</th>
+                            <th>DNI</th>
+                            <th>Mascota</th>
+                            <th>Propietario</th>
+                            <th>Email</th>
+                            <th>Telefono</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </div>
+            <div id="lblTotalReg" class="footer-table-fcp"></div>
+        </div>
+    </div>
+</div>
+<script type="text/javascript">
+    $.getScript("js/page/mantenimiento/solicitud.js")
+        .fail(function (jqxhr, settings, exception) {
+            alert("Error: No se ha cargando un complemento del sistema (servicio.js), porfavor actualize la pagina para poder cargar el complemento. " + exception);
+        });
+    //SCRIPT GENERAL
+    $.getScript("js/general.js")
+        .fail(function (jqxhr, settings, exception) {
+            alert("Error: No se ha cargando un complemento del sistema (general.js), porfavor actualize la pagina para poder cargar el complemento. " + exception);
+        });
+</script>
---
+++ PRESENTACION/page/mantenimiento/solicitud.aspx.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Services;
+using System.IO;
+using ENTIDAD;
+using NEGOCIOS;
+
+namespace PRESENTACION.page.mantenimiento
+{
+    public partial class solicitud : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        [WebMethod()]
+        public static object listarParametro(EGeneral objE)
+        {
+            ERespuestaJson objRespuesta = new ERespuestaJson();
+            try
+            {
+                if (HttpContext.Current.Session["userRump"] == null)
+                {
+                    objRespuesta.Error("Su sesión ha expirado, por favor vuelva a iniciar sesión");
+                    return objRespuesta;
+                }
+                List<EGeneral> objResultado = new List<EGeneral>();
+                objResultado = NParametro.listarParametro(objE);
+                if (objResultado.Count == 0)
+                {
+                    objRespuesta.Error("No se encontraron registros.");
+                }
+                else
+                {
+                    objRespuesta.Resultado = objResultado;
+                }
+            }
+            catch (Exception ex)
+            {
+                objRespuesta.Error(String.IsNullOrEmpty(ex.Message) ? ex.InnerException.Message : ex.Message);
+            }
+            return objRespuesta;
+        }
+
+
+        [WebMethod()]
+        public static object ListaUsuarioWM(EUsuario objE)
+        {
+            ERespuestaJson objRespuesta = new ERespuestaJson();
+            try
+            {
+                if (HttpContext.Current.Session["userRump"] == null)
+                {
+                    objRespuesta.Error("Su sesión ha expirado, por favor vuelva a iniciar sesión");
+                    return objRespuesta;
+                }
+
+                List<EUsuario> objResultado = new List<EUsuario>();
+
+                objResultado = NUsuario.listarUsuario(objE);
+
+                /*if (objResultado.Count == 0)
+                {
+                    objRespuesta.Error("No se encontraron registros.");
+                }
+                else
+                {
+                    objRespuesta.Resultado = objResultado;
+                }*/
+
+                objRespuesta.Resultado = objResultado;
+            }
+            catch (Exception ex)
+            {
+                objRespuesta.Error(String.IsNullOrEmpty(ex.Message) ? ex.InnerException.Message : ex.Message);
+            }
+            return objRespuesta;
+        }
+        [WebMethod()]
+        public static object ObtenerUsuarioWM(EUsuario objE)
+        {
+            ERespuestaJson objRespuesta = new ERespuestaJson();
+            try
+            {
+                if (HttpContext.Current.Session["userRump"] == null)
+                {
+                    objRespuesta.Error("Su sesión ha expirado, por favor vuelva a iniciar sesión");
+                    return objRespuesta;
+                }
+
+                EUsuario objResultado = new EUsuario();
+                objResultado = NUsuario.ObtenerUsuario(objE);
+                objRespuesta.Resultado = objResultado;
+            }
+            catch (Exception ex)
+            {
+                objRespuesta.Error(String.IsNullOrEmpty(ex.Message) ? ex.InnerException.Message : ex.Message);
+            }
+            return objRespuesta;
+        }
+        [WebMethod()]
+        public static object AnularUsuarioWM(EUsuario objE)
+        {
+            ERespuestaJson objRespuesta = new ERespuestaJson();
+            try
+            {
+                if (HttpContext.Current.Session["userRump"] == null)
+                {
+                    objRespuesta.Error("Su sesión ha expirado, por favor vuelva a iniciar sesión");
+                    return objRespuesta;
+                }
+
+                int objResultado = 0;
+
+                objResultado = NUsuario.AnularUsuario(objE);
+
+                if (objResultado == 0)
+                {
+                    objRespuesta.Error("No se pudo eliminar.");
+                }
+                else
+                {
+                    objRespuesta.Success("Se eliminó el usuario correctamente");
+                }
+            }
+            catch (Exception ex)
+            {
+                objRespuesta.Error(String.IsNullOrEmpty(ex.Message) ? ex.InnerException.Message : ex.Message);
+            }
+            return objRespuesta;
+        }
+        [WebMethod()]
+        public static object ActualizarUsuarioWM(EUsuario objE)
+        {
+            ERespuestaJson objRespuesta = new ERespuestaJson();
+            try
+            {
+                if (HttpContext.Current.Session["userRump"] == null)
+                {
+                    objRespuesta.Error("Su sesión ha expirado, por favor vuelva a iniciar sesión");
+                    return objRespuesta;
+                }
+
+                string objResultado = "";
+
+                if (objE.ID_ENCRIP != "")
+                {
+                    if (NUsuario.ActualizarUsuario(objE) > 0)
+                    {
+                        objResultado = objE.ID_ENCRIP;
+                    }
+                }
+                else
+                {
+                    objResultado = NUsuario.RegistrarUsuario(objE);
+
+                    if (objE.EMAIL.Contains("@"))
+                    {
+                        objE.ID_ENCRIP = objResultado;
+                        objE.TOKEN_ACTIVACION = objResultado;
+                        int objResultadoACtivacion = NUsuario.TokenActivoUsuario(objE);
+
+                        ECorreo correo = new ECorreo();
+                        correo.Para = objE.EMAIL;
+                        correo.Asunto = "Activación de Usuario";
+                        correo.Mensaje = "Active su cuenta ingresando al siguiente enlace:<br/>" +
+                            "<a href=\"https://rumpp.charpetechnology.com/active.aspx?user=" + objResultado + "\">ACTIVAR CUENTA</a>";
+                        correo.Enviar();
+                    }
+                }
+
+
+                if (objResultado == "")
+                {
+                    objRespuesta.Error("No se pudo actualizar.");
+                }
+                else
+                {
+                    objRespuesta.Resultado = objResultado;
+                    objRespuesta.Success("Se guardó la información correctamente");
+                }
+            }
+            catch (Exception ex)
+            {
+                objRespuesta.Error(String.IsNullOrEmpty(ex.Message) ? ex.InnerException.Message : ex.Message);
+            }
+            return objRespuesta;
+        }
+        [WebMethod()]
+        public static object InsertarFotoUsuarioWM(EUsuario objE)
+        {
+            ERespuestaJson objRespuesta = new ERespuestaJson();
+            try
+            {
+                if (HttpContext.Current.Session["userRump"] == null)
+                {
+                    objRespuesta.Error("Su sesión ha expirado, por favor vuelva a iniciar sesión");
+                    return objRespuesta;
+                }
+
+                string objResultado = "";
+                objE.FOTO = objE.EXTENSION;
+                objResultado = NUsuario.ActualizarFotoUsuario(objE);
+
+                if (objResultado == "")
+                {
+                    objRespuesta.Error("No se pudo actualizar.");
+                }
+                else
+                {
+                    objRespuesta.Resultado = objResultado;
+                    objRespuesta.Success("Se guardó la información correctamente");
+                }
+            }
+            catch (Exception ex)
+            {
+                objRespuesta.Error(String.IsNullOrEmpty(ex.Message) ? ex.InnerException.Message : ex.Message);
+            }
+            return objRespuesta;
+        }
+    }
+}
